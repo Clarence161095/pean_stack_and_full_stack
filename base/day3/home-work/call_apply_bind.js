1d1ceab0d832b8e867dabf80fa98864f64d751ea
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-08-16 21:35:13 +0900

    Review home-work

--- base/day3/home-work/call_apply_bind.js
+++ base/day3/home-work/call_apply_bind.js
@@ -1,55 +1,64 @@
 // call không tham số 
-const demo = {
-    name : "Linh",
-    age : 26
-}
-function Izuka(){
-  return {
-     key1: this.name,
-     key2: this.age
-  }
-}
-demo.Test = Izuka.call(demo);
-console.log(demo);
+// const demo = {
+//   name: "Linh",
+//   age: 26
+// }
+// function izuka() {
+//   return {
+//     key1: this.name,
+//     key2: this.age
+//   }
+// }
+// demo.Test = izuka.call(demo);
+// console.log(demo);
 
 
 //call có tham số 
-// const Izuka = {
-//     name: "Minaho",
-//     age : 25
+// const izuka = {
+//   name: "Minaho",
+//   age: 25
 // }
-// const Rin = function(test,demo){
-//     return this.name + " " + this.age + " " + test + " " +demo ;
+// const Rin = function (test, demo) {
+//   return this.name + " " + this.age + " " + test + " " + demo;
 // }
-// Izuka.Hobby = Rin.call(Izuka,"bat","chi");
+// // const Rin = (test, demo) => {
+// //   return this.name + " " + this.age + " " + test + " " + demo;
+// // }
+// izuka.Hobby = Rin.call(izuka, "bat", "chi");
 
-// console.log(Izuka);
+// console.log(izuka);
 
- 
+
 //apply
-// const Izuka = {
-//     name: "Anh",
-//     age: "Yeu"
-// }
-// const Naho = function(key1,key2,key3){
-//     return key1 + " " + key2;
-// }
-// Izuka.Hobby = Naho.apply(Izuka,["Em","nvd","pjk"])
-// console.log(Izuka);
-
-
-// const demo1 = {
-//     name : "Linh",
-//     age : 28,
-//     Rin: function(){
-//       return this.name + " " + this.age;
-//     }
+// const izuka = {
+//   name: "Anh",
+//   age: "Yeu"
 // }
 
-// const demo2 = {
-//     name: "Nhai",
-//     age:26
+// const Naho = function (...keys) {
+//   const [key1, key2, ...keyTemp] = keys
+//   console.log('this', this)
+//   console.log('keyTemp', keyTemp)
+//   return key1 + " " + key2;
 // }
 
-// demo2.Prop = demo1.Rin.bind(demo2)();
-// console.log(demo2);
+// Naho.apply(izuka, ["Em", "nvd", "pjk"]);
+// izuka.Hobby = Naho.apply(izuka, ["1", "2", "3", "4", "5"])
+// console.log(izuka);
+
+const demo1 = {
+  name: "Linh",
+  age: 28,
+  Rin: function () {
+    console.log(this.name + " " + this.age);
+  }
+}
+
+const demo2 = {
+  name: "Nhai",
+  age: 26
+}
+
+const newRin = demo1.Rin.bind(demo2);
+newRin();
+demo1.Rin();
