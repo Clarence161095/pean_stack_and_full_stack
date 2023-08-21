c3c1951d7ebfb86edc0f871be9a40d00d4f9d6c8
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-08-21 23:25:10 +0900

    update

--- base/day_6/redux-ngrx/src/app/counter/counter.component.ts
+++ base/day_6/redux-ngrx/src/app/counter/counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { action } from './+state/counter.actions'
+import { action } from './+state/counter.actions';
 
 @Component({
   selector: 'app-counter',
@@ -10,6 +10,7 @@ import { action } from './+state/counter.actions'
 })
 export class CounterComponent {
   count$: Observable<number>;
+  tuanCount = 0;
 
   constructor(private store: Store<{ count: number }>) {
     this.count$ = store.select('count');
@@ -17,13 +18,34 @@ export class CounterComponent {
 
   increment() {
     this.store.dispatch(action.tang());
+    this.tuanCount++;
+  }
+
+  plus5() {
+    // tạo 1 trigger cho plus5
+    // dispatch với payload action tang 5
+  }
+
+  minus10() {
+    // tạo 1 trigger cho minus10
+    // dispatch với payload action giam 10
+  }
+
+  plus(event: any) {
+    // tuong tu plus5 tuy nhiên giá trị playload được truyền vào tự input
+  }
+
+  minus(event: any) {
+    // tuong tu minus10 tuy nhiên giá trị playload được truyền vào tự input
   }
 
   decrement() {
     this.store.dispatch(action.giam());
+    this.tuanCount--;
   }
 
   reset() {
     this.store.dispatch(action.reset());
+    this.tuanCount = 0;
   }
 }
--- base/day_6/redux-ngrx/src/app/home/home.component.ts
+++ base/day_6/redux-ngrx/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  countState: Observable<any>;
 
+  constructor(private store: Store<any>) {
+    this.countState = store.select('count');
+  }
 }
