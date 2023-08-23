21cf8cf3921cffb19d649baad7e559e6614937dc
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-08-23 05:20:17 -0700

    Day 6 (#16)
    * init day 6
    
    * update
    
    * linh commit 2308
    
    * Update day 6
    
    * Resolve Confict
    
    ---------
    
    Co-authored-by: “linhPham98” <“linhthusinh98@gmail”>

--- base/day_6/redux-ngrx/src/app/counter/+state/counter.actions.ts
+++ base/day_6/redux-ngrx/src/app/counter/+state/counter.actions.ts
@@ -1,7 +1,12 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 export const action = {
   tang: createAction('[Counter Component] Tang gia tri counter + 1 :D'),
-  giam: createAction('[Counter Component] Gia gia tri counter - 1 :('),
+  giam: createAction('[Counter Component] Giam gia tri counter - 1 :('),
   reset: createAction('[Counter Component] Thuc hien Reset lai counter => 0'),
+  tang5: createAction('Tang 5'),
+  giam10: createAction('Giam 10'),
+  tangX: createAction('Tang X', props<{ value: number }>()),
+  giamX: createAction('Giam X', props<{ value: number }>()),
 };
+
--- base/day_6/redux-ngrx/src/app/counter/+state/counter.reducer.ts
+++ base/day_6/redux-ngrx/src/app/counter/+state/counter.reducer.ts
@@ -6,8 +6,10 @@ export const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
   on(action.tang, (state) => state + 1),
-  on(action.giam, function (state: any) {
-    return state - 1;
-  }),
-  on(action.reset, (state) => initialState)
+  on(action.giam, (state) => state - 1),
+  on(action.reset, () => initialState),
+  on(action.tang5, (state) => state + 5),
+  on(action.giam10, (state) => state - 10),
+  on(action.tangX, (state, { value }) => state + value),
+  on(action.giamX, (state, { value }) => state - value),
 );
