8833f34b6441d500749e6f80d1884d7b9574890a
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-08-28 21:39:07 +0900

    update day 8

--- base/day_8/home-work/homework.js
+++ base/day_8/home-work/homework.js
@@ -1,15 +1,15 @@
-// //1. Class là gì và nó khác gì với object
-// // #Class : 
-// // - 1 template để tạo Object 
-// // - Chứa thuộc tính + phương thức mà Object được tạo ra sẽ có 
-// // - Class mô tả cách đối tượng hoạt động + chứa thông tin gì
-// // - Không làm việc trực tiếp với chương trình
-// // #Object: 
-// // - 1 phiên bản cụ thể được tạo ra dựa trên class
-// // - Object sẽ có các thuộc tính được định nghĩa trong class
-// // // - Mỗi Object có thể có giá trị thuộc tính khác nhau tùy vào cách đối tượng
-// // được tạo 
-// // - làm việc trực tiếp với chương trình bằng cách gọi phương thức và truy cập thuộc tính
+// 1. Class là gì và nó khác gì với object
+// #Class : 
+// - 1 template để tạo Object 
+// - Chứa thuộc tính + phương thức mà Object được tạo ra sẽ có 
+// - Class mô tả cách đối tượng hoạt động + chứa thông tin gì
+// - Không làm việc trực tiếp với chương trình
+// #Object: 
+// - 1 phiên bản cụ thể được tạo ra dựa trên class
+// - Object sẽ có các thuộc tính được định nghĩa trong class
+// - Mỗi Object có thể có giá trị thuộc tính khác nhau tùy vào cách đối tượng
+// được tạo 
+// - làm việc trực tiếp với chương trình bằng cách gọi phương thức và truy cập thuộc tính
 
 // class Car{
 //     constructor(name,price = 0){
@@ -21,6 +21,25 @@
 // const car2 =  new Car("Vios");
 // console.log(car1);
 // console.log(car2);
+
+const app = {
+  db: {
+    sinhVien: []
+  },
+  use(...prompt) {
+    const [route = '/', callBack = (res, req) => {
+
+    }] = prompt;
+  },
+  get, post, put: this.use,
+  main() {
+    console.log('Chương trình đã khởi động');
+    app.get('getNameSinhVien', (res, req) => {
+
+    })
+    return this;
+  }
+}.main();
 
 // //2. Ôn lại Destructuring
 // //==> tách 1 array or object thành các giá trị riêng biệt
