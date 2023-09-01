60965b7e889fc423b21853016833c37f159f3426
“linhPham98” <linhthusinh98@gmail>
2023-09-01 21:15:13 +0900

    Bt1 up

---
+++ base/day_9/homework/B1.js
@@ -0,0 +1,14 @@
+
+// B1:
+function sayHi(){
+    console.log(name);
+    console.log(age);
+    var name = "Lydia";
+    let age = 21;
+}
+sayHi();
+// -Dap an D: undefined va ReferenceError
+// -undefined: var có tồn tại 1 cơ chế cho phép đưa phần khai báo lên đầu dù vị trí
+//khai báo được đặt tại đâu và gán cho bằng giá trị khởi tạo mặc định kxd
+//ReferenceError: do let khi khởi tạo không được gán cho giá trị mặc định nên let sẽ
+//trả về ReferenceError thay vì kxd
