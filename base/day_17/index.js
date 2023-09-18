4c65ce60cb9ab208cb67d10bcead3af1a4cbc222
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-09-18 23:13:20 +0900

    update day 17

--- base/day_17/index.js
+++ base/day_17/index.js
@@ -1,11 +1,20 @@
+function getShape() {
+  this.radius = 20;
+  return {
+    radius: 1000,
+    diameter() {
+      return this.radius;
+    },
+    perimeter: () => this.radius
+  };
+}
 
+const shape1 = getShape()
+const shape2 = getShape()
+shape1.radius = 5;
+// shape2.radius = 10;
 
-
-
-const query = `select e.name, e.email, count(p.project_id) as total_projects
-            from employees e
-            join employee_projects ep on e.employee_id = ep.employee_id
-            join projects p on ep.project_id = p.project_id
-            group by e.name, e.email
-            having count(p.project_id) > 5
-            order by total_projects desc`
+console.log(shape1.diameter());
+console.log(shape1.perimeter());
+console.log(shape2.diameter());
+console.log(shape2.perimeter());
