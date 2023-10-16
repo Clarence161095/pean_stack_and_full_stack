a72844d05591ef5d0f613cc2f64719e0d2e31121
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-10-16 23:40:46 +0900

    day 28

--- base/day_28/reduce_1.js
+++ base/day_28/reduce_1.js
@@ -7,5 +7,21 @@ function callbackfn(finalResult, currentData) {
 }
 
 // const callbackfn = (finalResult, currentData) => finalResult + currentData
+// console.log("tongReduce: ", array.reduce(callbackfn, 0));
 
-console.log(array.reduce(callbackfn, 0));
+console.log("tongReduce: ", array.reduce((acc, cur) => acc + cur, 0));
+
+let tongEach = 0;
+array.forEach(element => {
+  tongEach = callbackfn(tongEach, element)
+});
+console.log("tongEach: ", tongEach);
+delete tongEach;
+
+(() => {
+  let tongMap = 0;
+  array.map(e => {
+    tongMap += e;
+  })
+  console.log("tongMap: ", tongMap);
+})()
