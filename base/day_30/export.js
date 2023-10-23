5677474710df128639a1f712b89cfb3ae4feee79
Izuka Naho <izukanaho@JP.local>
2023-10-23 22:57:26 +0900

    mess

---
+++ base/day_30/export.js
@@ -0,0 +1,3 @@
+// sum.js
+console.log('running sum.js');
+export const sum = (a, b) => a + b;
---
+++ base/day_30/import.js
@@ -0,0 +1,5 @@
+// index.js
+console.log('running index.js');
+import { sum } from './sum.js';
+console.log(sum(1, 2));
+
