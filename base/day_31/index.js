db9c8e636fd4baccb2c751f09531955944722b80
Izuka Naho <izukanaho@JP.local>
2023-10-25 21:48:54 +0900

    cmd

--- base/day_31/index.js
+++ base/day_31/index.js
@@ -1,3 +1,26 @@
-const { name = "Tuan" } = { name: "Lydia" };
+// const { name = "Tuan" } = { name: "Lydia" };
 
-console.log(name);
+// console.log(name);
+
+
+const add = () => {
+    const cache = {};
+    return num => {
+      if (num in cache) {
+        return `From cache! ${cache[num]}`;
+      } else {
+        const result = num + 10;
+        cache[num] = result;
+        return `Calculated! ${result}`;
+      }
+    };
+  };
+  
+  const addFunction = add();
+  const addFunction2 = add();
+  console.log(addFunction(10));
+  console.log(addFunction(10));
+  console.log(addFunction(5 * 2));
+  console.log(addFunction2(10));
+  console.log(addFunction2(10));
+  console.log(addFunction2(5 * 2));
