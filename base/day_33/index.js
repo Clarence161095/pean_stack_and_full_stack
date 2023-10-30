b987c79404a27234aeb9fcc1e4d03cce18c7b0e0
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-10-30 23:34:51 +0900

    30/10

--- base/day_33/index.js
+++ base/day_33/index.js
@@ -1,4 +1,12 @@
 const tableInfo = {
+  books: [1, 2, 3, 4, 5],
+  members: [],
+  borrowings: [],
+  employee: [1, 2],
+  greenLive: [1]
+}
+
+const tableAttribute = {
   books: {
     tieuDe: "string",
     tacGia: "string",
@@ -7,21 +15,18 @@ const tableInfo = {
     soLuongDaMuon: "number",
     tomTat: "string",
     thumbnailUrl: "string",
-    listID: [1, 2, 3, 4, 5]
   },
   members: {
     tenDocGia: "string",
     soTheDocGia: "string",
     diaChi: "string",
     soLuongSachDaMuon: "number",
-    listID:[1,2]
   },
   borrowings: {
     soTheDocGia: "string",
     idSach: "string",
     ngayMuon: "date",
     ngayTra: "date",
-    listID:[3,4]
   },
   employee: {
     maNhanVien: "string",
@@ -59,14 +64,14 @@ const tableValue = {
   "employee_tenNhanVien_1": "linhpt",
   "employee_maNhanVien_2": "nv002",
   "employee_tenNhanVien_2": "tuanna",
-  "members_tenDocGia_1" : "Sach 1",
-  "members_soTheDocGia_1" : "Tac gia A",
+  "members_tenDocGia_1": "Sach 1",
+  "members_soTheDocGia_1": "Tac gia A",
   "members_diaChi_1": "2000",
-  "members_soLuongSachDaMuon_1" : "5",
-  "members_tenDocGia_2" : "Sach 2",
-  "members_soTheDocGia_2" : "Tac gia B",
+  "members_soLuongSachDaMuon_1": "5",
+  "members_tenDocGia_2": "Sach 2",
+  "members_soTheDocGia_2": "Tac gia B",
   "members_diaChi_2": "2000",
-  "members_soLuongSachDaMuon_2" : "5",
+  "members_soLuongSachDaMuon_2": "5",
   "borrowings_soTheDocGia_3": "1",
   "borrowings_idSach_3": "2",
   "borrowings_ngayMuon_3": "2023",
@@ -78,30 +83,15 @@ const tableValue = {
 };
 
 let result = ''
-let result2 = '';
-let result3 = '';
 
 result += 'id	| Tiêu đề	| Tác giả	| Năm xuất bản	| Số lượng	| số lượng đã mượn |\n';
 result += '--	| ------	| --------| ------------	| --------	| ---------------- |\n';
 
-result2 += 'id	| Ten doc gia	|So The Doc Gia	| Dia Chi	| số lượng sach đã mượn |\n';
-result2 += '--	| ------	| --------| ------------	| --------	| ---------------- |\n';
-
-result3 += 'id	|So The Doc Gia	| ma sach	| ngay muon | ngay tra|\n';
-result3 += '--	| ------	| --------| ------------	| --------	| ---------------- |\n';
-
-tableInfo.books.listID.forEach(id => {
+tableInfo.books.forEach(id => {
   result += `${id} | ${tableValue[`books_tieuDe_${id}`]} | ${tableValue[`books_tacGia_${id}`]} | ${tableValue[`books_namXuatBan_${id}`]} | ${tableValue[`books_soLuong_${id}`]} | ${tableValue[`books_soLuongDaMuon_${id}`]}\n`;
 })
 
-tableInfo.members.listID.forEach(v => {
-  result2 += `${v} | ${tableValue[`members_tenDocGia_${v}`]} | ${tableValue[`members_soTheDocGia_${v}`]} | ${tableValue[`members_diaChi_${v}`]} | ${tableValue[`members_soLuongSachDaMuon_${v}`]}\n`;
-})
+// console.log(result);
+console.log(tableValue["borrowings_ngayTra_3"]);
 
-tableInfo.borrowings.listID.forEach(v => {
-  result3 += `${v} | ${tableValue[`borrowings_soTheDocGia_${v}`]} | ${tableValue[`borrowings_idSach_${v}`]} | ${tableValue[`borrowings_ngayMuon_${v}`]} | ${tableValue[`borrowings_ngayTra_${v}`]}\n`;
-})
-
-console.log(result);
-console.log(result2);
-console.log(result3);
+document.findById("borrowings_ngayTra_3")
