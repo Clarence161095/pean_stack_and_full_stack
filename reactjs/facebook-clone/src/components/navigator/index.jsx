ccbad1fb64b517db95109a60e7ce8ea352898f2c
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2023-12-06 23:43:09 +0900

    Day 49 (#73)
    * day 49
    
    * linh cmt
    
    * delete file
    
    * update 49
    
    ---------
    
    Co-authored-by: “linhPham98” <“linhthusinh98@gmail”>

--- reactjs/facebook-clone/src/components/navigator/index.jsx
+++ reactjs/facebook-clone/src/components/navigator/index.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { SVG } from '../../constants/svg'
+import CustomSVG from '../ui/custom-svg'
 import classes from './index.module.scss'
 
 export default function Navigator() {
+  const [searchIcon, setSearchIcon] = useState(true)
+
   return (
     <div className={classes.nav}>
       <div className={classes.right}>
         <img src="assets/facebook_logo_icon.png" alt="Facebook Logo" />
-        <div>search</div>
+        <div className={classes.search}>
+          <input onFocus={() => setSearchIcon(false)} onBlur={() => setSearchIcon(true)} />
+          <span className={searchIcon ? '' : classes.hidden}>
+            <CustomSVG svgCode={SVG.search} />
+          </span>
+        </div>
       </div>
       <nav>
         <ul className={classes.middle}>
           <li>
-            feed
+            <CustomSVG svgCode={SVG.home} />
           </li>
           <li>
-            marketplace
+            <CustomSVG svgCode={SVG.play} />
+          </li>
+          <li>
+            <CustomSVG svgCode={SVG.shop} />
+          </li>
+          <li>
+            <CustomSVG svgCode={SVG.group} />
+          </li>
+          <li>
+            <CustomSVG svgCode={SVG.game} />
           </li>
         </ul>
       </nav>
       <div className={classes.left}>
-        <div>chat</div>
-        <img src="assets/my_avatar.png" alt='My Avatar'/>
+        <CustomSVG svgCode={SVG.menu} />
+        <CustomSVG svgCode={SVG.message} />
+        <CustomSVG svgCode={SVG.notification} />
+        <img src="assets/my_avatar.png" alt='My Avatar' />
       </div>
     </div>
   )
