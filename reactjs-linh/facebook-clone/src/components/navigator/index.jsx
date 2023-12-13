36a98b2d14a81a0d977e6ac55864c2c9c899b649
“linhPham98” <linhthusinh98@gmail>
2023-12-13 21:35:17 +0900

    linh cmt

--- reactjs-linh/facebook-clone/src/components/navigator/index.jsx
+++ reactjs-linh/facebook-clone/src/components/navigator/index.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./index.module.scss";
 import CustomSVG from "../ui/custom-svg";
 import { SVG } from "../../constants/svg";
 import InputWithIcon from "../ui/button-icon";
 
 export default function Navigator() {
+  // let isVisibleLogo = true; khong duoc dung nhu vay
+  // Su dung co che cua react nhu sau
+  const [isVisibleLogo, setIsVisibleLogo] = useState(true); //state cho icon FB
+  function focusHandler() {
+    console.log("Click Vao", isVisibleLogo);
+    setIsVisibleLogo(false);
+    //isVisibleLogo = false; khong dc dung nhu nay
+    //Do se khong update lai gia tri cho isVisibleLogo
+  }
   return (
     <div className={classes.nav}>
-      <div className={classes.right}>
-        <img src="assets/fb.png" alt="Facebook Logo" />
-        <InputWithIcon svgCode={SVG.home} />
+      <div className={classes.left}>
+        {isVisibleLogo ? (
+          <img src="assets/fb.png" alt="Facebook Logo" />
+        ) : (
+          //CustomSVG co 1 div .svg
+          <span className={classes.arrow}>
+            <CustomSVG svgCode={SVG.arrow} type='span'/>
+          </span>
+        )}
+        <InputWithIcon
+          svg={SVG.search}
+          placeholdersomething="Tim kiem tren Facebook" //thay gtri moi cho placeholders
+          onFocusSomething={focusHandler}
+          //TODO: truyen focusHandler nay la callback ?
+          onBlurSomething={() => {
+            console.log("click Ra", isVisibleLogo);
+            setIsVisibleLogo(true);
+          }}
+        />
       </div>
       <nav>
         <ul className={classes.middle}>
@@ -30,7 +55,7 @@ export default function Navigator() {
           </li>
         </ul>
       </nav>
-      <div className={classes.left}>
+      <div className={classes.right}>
         <CustomSVG svgCode={SVG.menu} />
         <CustomSVG svgCode={SVG.messenger} />
         <CustomSVG svgCode={SVG.notificate} />
