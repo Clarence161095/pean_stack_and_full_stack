daa527a8b53287cb0b5a7928efe2769270bb680a
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-01-10 22:48:40 +0900

    Add Reactjs resources and update Home component

--- pern_stack_template/dashboard/src/pages/home/Home.jsx
+++ pern_stack_template/dashboard/src/pages/home/Home.jsx
@@ -1,44 +1,34 @@
-import { useEffect, useRef, useState } from "react";
+import { useReducer, useState } from "react";
+
+// TODO: I want to save the value of age when I change page
+// You can combine useReducer and useContext to save the value of age when you change page
+function reducer(state, action) {
+  switch (action.type) {
+    case 'TANG_TUOI':
+      return { age: state.age + 1 };
+    case 'GIAM_TUOI':
+      return { age: state.age - 1 };
+    default:
+      return state;
+  }
+}
 
 export default function Home() {
-  const [count1, setCount] = useState(0);
-  const count2 = useRef(0);
-  const [count3, setCount3] = useState(0);
-
-  // This will be called when count1, count3 change
-  useEffect(() => {
-    // The client type something in input, and we want to send request to server
-    console.log('onChange Home', count1, count2.current, count3);
-  });
-
-  // This will be called only once when component is mounted
-  useEffect(() => {
-    // Fetch data from server
-    console.log('onInit Home', count1, count2.current, count3);
-  }, []);
-
-  useEffect(() => {
-    console.log('onChange count3', count3);
-  }, [count3]);
-
-  useEffect(() => {
-    // This will be called when component is destroyed, you can clear interval, clear timeout, clear listener here
-    return () => {
-      console.log('onDestroy Home', count1, count2.current, count3);
-    }
-  }, []);
+  const [state, dispatch] = useReducer(reducer, { age: 18 });
+  const [state2, setState2] = useState({ age: 18 });
 
   return (
     <>
-      <h1>Learn about useEffect concept</h1>
-      <p>This is Home page.</p>
-      <button onClick={() => setCount(count1 + 1)}>Click me 1</button>
-      <br />
-      <button onClick={() => {
-        count2.current++;
-      }}>Click me 2</button>
-      <br />
-      <button onClick={() => setCount3(count3 + 1)}>Click me 3</button>
+      <div>
+        <button onClick={() => dispatch({ type: 'GIAM_TUOI' })}>-</button>
+        {state.age}
+        <button onClick={() => dispatch({ type: 'TANG_TUOI' })}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setState2({ age: state2.age - 1 })}>-</button>
+        {state2.age}
+        <button onClick={() => setState2({ age: state2.age + 1 })}>+</button>
+      </div>
     </>
   )
 }
