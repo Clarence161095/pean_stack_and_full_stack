9533dc78263d75cd617501278d1b9c95046bb0ca
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-06-10 23:28:59 +0900

    day 127

--- deploy/simple-app/tuan.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello, world!");
-
-setInterval(() => {
-  let date = new Date();
-  console.log(
-    "Time: ",
-    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
-  );
-}, 1000);
