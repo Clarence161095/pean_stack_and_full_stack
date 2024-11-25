e307b34cae6ac2616a845e2b3c28e6e5cd0ff4a7
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-11-25 21:18:19 +0900

    feat: Add token expiration check in authentication middleware

--- protected-route-app/server/src/middlewares/authenticated.middleware.ts
+++ protected-route-app/server/src/middlewares/authenticated.middleware.ts
@@ -8,7 +8,10 @@ export const decodeLoginUser = (req: any, res: any, next: any) => {
   if (req.signedCookies.token) {
     try {
       req.loginUser = jwt.verify(req.signedCookies.token, envConfig.JWT_SECRET);
-      // check if token is expired
+      if (req.loginUser.exp < new Date().getTime() / 1000) {
+        res.clearCookie('token');
+        return res.status(401).json({ message: 'Token expired' });
+      }
     } catch (error) {
       return res.status(401).json({ message: 'Invalid token' });
     }
